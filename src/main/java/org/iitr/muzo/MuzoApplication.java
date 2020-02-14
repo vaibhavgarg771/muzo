b389ce1cdb00f841756efb413bdf12f25c51466e
Vaibhav Garg <vaibhav.garg771@gmail.com>
2020-02-14 22:40:33 +0530

    initial commit

---
+++ src/main/java/org/iitr/muzo/MuzoApplication.java
@@ -0,0 +1,13 @@
+package org.iitr.muzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzoApplication.class, args);
+	}
+
+}
