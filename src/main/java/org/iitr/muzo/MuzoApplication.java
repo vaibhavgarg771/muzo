33b8e8bf1d24ac9c17d10c8e7e8fcac137356cf9
Vaibhav Garg <vaibhav.garg771@gmail.com>
2020-02-17 01:52:19 +0530

    addressing review comments

--- src/main/java/org/iitr/muzo/MuzoApplication.java
+++ src/main/java/org/iitr/muzo/MuzoApplication.java
@@ -3,7 +3,7 @@ package org.iitr.muzo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.iitr.muzo.dao", "org.iitr.muzo.models", "org.iitr.muzo.services", "org.iitr.muzo.controller"})
+@SpringBootApplication
 public class MuzoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/iitr/muzo/configuration/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package org.iitr.muzo.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
